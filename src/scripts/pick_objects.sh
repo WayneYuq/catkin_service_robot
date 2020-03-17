5fbe4642fcf6c5cd39e51d66b768c6c5665a4a8e
Wayne Yu <3154816042@qq.com>
2020-03-17 10:09:53 -0700

    Sending goal by code

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -2,8 +2,8 @@
 
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm -e " rosrun pick_objects pick_objects"
